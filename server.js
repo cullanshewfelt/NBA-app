0e3af66d3983197b42370c820da6bc471ce56e48
Cullan Shewfelt <cullan.shewfelt@gmail.com>
2019-10-28 13:20:22 -0700

    games as objects function

--- server.js
+++ server.js
@@ -7,10 +7,25 @@ console.log(today)
 const getTodaysGames = () => {
   fetch(`https://stats.nba.com/stats/scoreboard/?gamedate=${today}&LeagueID=00&DayOffset=0`, (response) => {
     response = response.data.resultSets;
-    console.log(`There are ${response[0].rowSet.length} Games Today`)
-    console.log(10, response[0].name);
-    console.log(11, response[0].headers);
-    console.log(12, response[0].rowSet);
+    console.log(`There are ${response[0].rowSet.length} Games Today`);
+
+    let gamesAsObjects = response[0].rowSet.map((columns, i) => {
+      return columns.map((column, j) => {
+        let obj = {};
+        obj[response[0].headers[j]] = column;
+        return obj;
+      })
+    }).reduce((acc, curr) => {
+      let newObj = {};
+      curr.forEach(x => {
+        newObj = { ...newObj, ...x };
+      });
+      return [...acc, newObj];
+    }, [])
+
+    // console.log(10, response[0].name);
+    // console.log(11, response[0].headers);
+    console.log(12, gamesAsObjects);
   })
 };
 
