e309b7c261d90f5a043fdd11e62d6a3d4ae2fc4f
Cullan Shewfelt <cullanrocks@gmail.com>
2019-10-28 21:42:44 -0700

    pulling

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+
 const axios = require("axios").default;
 const moment = require("moment");
 const today = moment().format("MM/DD/YYYY")
 console.log("NBA app");
 console.log(today)
 
-axios.get(`https://stats.nba.com/stats/scoreboard/?gamedate=${today}&LeagueID=00&DayOffset=0`)
-  .then(response => {
-    console.log(response.data.resultSets)
-  })
-  .catch(error => console.log(error));
+
+const getTodaysGames = (today) => {
+  fetch(`https://stats.nba.com/stats/scoreboard/?gamedate=${today}&LeagueID=00&DayOffset=0`, (response) => {
+    console.log(response.data.resultSets[0].name);
+    console.log(response.data.resultSets[0].headers);
+    console.log(response.data.resultSets[0].rowSet);
+  });
+}
+
+const fetch = (url, callback) => {
+  axios.get(url)
+  .then(response => callback(response))
+  .catch(error => console.log(error))
+};
+
+getTodaysGames(today);
